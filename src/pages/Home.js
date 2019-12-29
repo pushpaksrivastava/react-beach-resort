28b3dde46195f1fab6000268a7a2337694deacb2
pushpak srivastava <pushpaksrivastava01@gmail.com>
2019-12-30 02:14:15 +0530

    React beach App

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
+const Home = () => {
+  return (
+    <>
+      <Hero>
+        <Banner title="Luxurious rooms" subtitle="Delux Rooms starting $299">
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
+    </>
+  );
+};
+export default Home;
